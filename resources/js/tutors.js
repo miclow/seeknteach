5a701e0984e1c1431642ac6ac7733f8857bb5777
Michelle Low <michellelow@Michelles-MacBook-Pro.local>
2016-04-17 13:44:31 +1000

    seek and teach prototype

---
+++ resources/js/tutors.js
@@ -0,0 +1,143 @@
+window.tutors = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "language": "Arabic",
+        "description": "<h3>Rhys</h3><p>I'm a native English speaker</p><p>Availability: Friday (1pm - 3pm)</p><button class='connect'>Connect</button>",
+          'marker-color': '#000',
+                'marker-symbol': 'star-stroked'
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.9539566040039,
+          -37.8127675575702
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "description": "<h3>Kristin</h3><p>12 years English teaching experience in Australia</p><p>Availability: Monday - Friday (8am - 3pm)</p><button class='connect'>Connect</button>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.96434211730957,
+          -37.81378466793125
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "description": "<h3>Olly</h3><p>5 years English teaching experience in Australia</p><p>Availability: Monday - Friday (8am - 3pm)</p><button class='connect'>Connect</button>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.97103691101074,
+          -37.815412015363556
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "description": "<h3>Asad</h3><p>I'm a native Persian speaker. I have intermediate English level. </p><p>Availability: Friday (1pm - 3pm)</p><button class='connect'>Connect</button>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.94563102722168,
+          -37.8026635004523
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "description": "<h3>Rhys</h3><p>I'm a native English speaker.</p><p>Availability: Friday (1pm - 3pm)</p><button class='connect'>Connect</button>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.96193885803223,
+          -37.797983972103374
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "description": "<h3>Thruong</h3><p>I'm a native Vietnamese speaker.</p><p>Availability: Friday (1pm - 3pm)</p><button class='connect'>Connect</button>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.96185302734375,
+          -37.826327876899036
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "language": "Arabic",
+        "description": "<h3>Wong</h3><p>I'm a native English speaker.</p><p>Availability: Friday (1pm - 3pm)</p><button class='connect'>Connect</button>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.96631622314453,
+          -37.82144644892643
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "language": "Arabic",
+        "description": "<h3>Chris</h3><p>I'm a native Arabic speaker.</p><p>Availability: Friday (1pm - 3pm)</p><button class='connect'>Connect</button>"
+
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.97695922851562,
+          -37.78808138412046
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "language": "Farsi",
+        "description": "<h3>AJ</h3><p>I'm a native English speaker.</p><p>Availability: Friday (1pm - 3pm)</p><button class='connect'>Connect</button>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.95086669921875,
+          -37.76474401178003
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "language": "Farsi",
+        "description": "<h3>Shamed</h3><p>I'm a native Persian speaker.</p><p>Availability: Friday (1pm - 3pm)</p><button class='connect'>Connect</button>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.9199676513672,
+          -37.785910776551354
+        ]
+      }
+    }
+  ]
+};
